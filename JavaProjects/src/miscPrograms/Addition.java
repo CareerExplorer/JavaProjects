5607ee9d789c2e134e6868d157c99b8fa6431280
user <user@user-PC>
2017-07-09 14:40:25 +0530

    Addition Java file is added

---
+++ JavaProjects/src/miscPrograms/Addition.java
@@ -0,0 +1,9 @@
+package miscPrograms;
+
+public class Addition {
+	public static void main(String args[]){
+		//System.out.println("Welcome to Java");
+		int a=1,b=2;
+		System.out.println(a+b);
+	}
+}
