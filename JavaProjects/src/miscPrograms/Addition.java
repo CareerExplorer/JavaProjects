ad633a51fff67cc4d87d26caa711311e863da560
user <user@user-PC>
2017-07-09 14:44:11 +0530

    Message Change

--- JavaProjects/src/miscPrograms/Addition.java
+++ JavaProjects/src/miscPrograms/Addition.java
@@ -5,6 +5,6 @@ public class Addition {
 		//System.out.println("Welcome to Java");
 		int a=1,b=2;
 		System.out.println(a+b);
-		System.out.println("Welcome");
+		System.out.println("Welcome to CEG");
 	}
 }
