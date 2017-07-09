fe68455114e410a6e173067dac0ec10c4df4ffe8
user <user@user-PC>
2017-07-09 14:38:07 +0530

    Basic Java Programs

---
+++ JavaProjects/src/miscPrograms/Subtraction.java
@@ -0,0 +1,9 @@
+package miscPrograms;
+
+public class Subtraction {
+	public static void main(String args[]){
+		//System.out.println("Welcome to Java");
+		int a=10,b=5;
+		System.out.println(a-b);
+	}
+}
